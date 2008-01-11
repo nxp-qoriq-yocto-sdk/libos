f3bfdffdbdf8b6a4b8007614741d42c1b0c61435
Stuart Yoder <stuart.yoder@freescale.com>
2008-01-11 09:56:40 -0600

    add byte_channel_send hypercall wrapper

--- include/libos/hcalls.h
+++ include/libos/hcalls.h
@@ -1,12 +1,16 @@
 #ifndef LIBOS_HCALLS_H
 #define LIBOS_HCALLS_H
 
-#define FH_CPU_WHOAMI           0x0
-#define FH_LPAR_GET_STATUS      0x7
+#define FH_CPU_WHOAMI		0x0
+#define FH_LPAR_GET_STATUS	0x7
+#define FH_BYTE_CHANNEL_SEND	0x10
 
 int32_t fh_cpu_whoami(uint32_t *cpu_index);
 
 int32_t fh_lpar_get_status(int lpar_id, uint32_t *lpar_status,
         uint32_t *num_cpus, uint32_t *mem_size);
+
+int32_t fh_byte_channel_send(uint32_t handle, int32_t count,
+	uint32_t str0, uint32_t str1, uint32_t str2, uint32_t str3);
 
 #endif
--- lib/hcall_wrapper.c
+++ lib/hcall_wrapper.c
@@ -2,13 +2,17 @@
 #include <libos/libos.h>
 #include <libos/hcalls.h>
 
+long hcall_ret0(uint32_t arg0,uint32_t arg1,uint32_t arg2,
+   uint32_t arg3,uint32_t arg4,uint32_t arg5,
+   uint32_t arg6,uint32_t arg7,int32_t token);
+
 long hcall_ret1(uint32_t arg0,uint32_t arg1,uint32_t arg2,
    uint32_t arg3,uint32_t arg4,uint32_t arg5,
-   uint32_t arg6,uint32_t arg7,long token,uint32_t *retbuf);
+   uint32_t arg6,uint32_t arg7,int32_t token,uint32_t *retbuf);
 
 long hcall_ret3(uint32_t arg0,uint32_t arg1,uint32_t arg2,
    uint32_t arg3,uint32_t arg4,uint32_t arg5,
-   uint32_t arg6,uint32_t arg7,uint32_t token,uint32_t *retbuf);
+   uint32_t arg6,uint32_t arg7,int32_t token,uint32_t *retbuf);
 
 
 int32_t fh_cpu_whoami(uint32_t *cpu_index)
@@ -34,6 +38,16 @@ int32_t fh_lpar_get_status(int lpar_id, uint32_t *lpar_status,
 	*lpar_status = retbuf[0];
 	*num_cpus = retbuf[1];
 	*mem_size = retbuf[2];
+
+	return status;
+}
+
+int32_t fh_byte_channel_send(uint32_t handle, int32_t count,
+	uint32_t str0, uint32_t str1, uint32_t str2, uint32_t str3)
+{
+	long status;
+
+	status = hcall_ret0(handle,count,str0,str1,str2,str3,0,0,FH_BYTE_CHANNEL_SEND);
 
 	return status;
 }
