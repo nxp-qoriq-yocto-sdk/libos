2f91237c0cd109b946ad20f52f43cfc552bafd13
Scott Wood <scottwood@freescale.com>
2008-01-16 10:26:09 -0600

    Add missing mp.c.
    Signed-off-by: Scott Wood <scottwood@freescale.com>

---
+++ lib/mp.c
@@ -0,0 +1,48 @@
+/*
+ * Multiprocessing support
+ *
+ * Copyright (C) 2008 Freescale Semiconductor, Inc.
+ * Author: Scott Wood <scottwood@freescale.com>
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN
+ * NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <libos/percpu.h>
+#include <libos/io.h>
+
+extern int secondary_start;
+
+int start_secondary_spin_table(struct boot_spin_table *table, int num,
+                               cpu_t *cpu, entry_t entry, void *arg)
+{
+	printf("table %p addr %lx pir %lx\n", table, table->addr, table->pir);
+
+	if (table->pir != num)
+		return -1;
+
+	table->r4 = (unsigned long)arg;
+	table->r3 = (unsigned long)cpu;
+	table->r7 = (unsigned long)entry;
+
+	// FIXME 64-bit
+	out32((uint32_t *)&table->addr, (uint32_t)&secondary_start - PHYSBASE);
+	return 0;
+}
