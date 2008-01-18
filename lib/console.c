3bba05a36b3e66ba137238c748120abc4263572a
Scott Wood <scottwood@freescale.com>
2008-01-18 14:54:19 -0600

    Fix locking in console.c.
    Previously, only printf() was locked; calls to other functions such as
    puts() (which can be generated automatically by GCC from printf() calls
    without arguments) bypassed the locking.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>

--- lib/console.c
+++ lib/console.c
@@ -5,13 +5,13 @@
 #include <libos/console.h>
 
 extern struct console_calls console;
+static uint32_t console_lock;
 
 void console_init(void)
 {
-
 }
 
-int putchar(int c)
+static int __putchar(int c)
 {
 	if (c == '\n')
 		console.putc('\r');
@@ -20,17 +20,36 @@ int putchar(int c)
 	return c;
 }
 
-void puts_len(const char *s, int len)
+int putchar(int c)
+{
+	spin_lock(&console_lock);
+	int ret = __putchar(c);
+	spin_unlock(&console_lock);
+	return ret;
+}
+
+static void __puts_len(const char *s, int len)
 {
 	while (*s && len--)
-		putchar(*s++);
+		__putchar(*s++);
+}
+
+void puts_len(const char *s, int len)
+{
+	spin_lock(&console_lock);
+	__puts_len(s, len);
+	spin_unlock(&console_lock);
 }
 
 int puts(const char *s)
 {
-	puts_len(s, INT_MAX);
+	spin_lock(&console_lock);
+
+	__puts_len(s, INT_MAX);
 	console.putc('\r');
 	console.putc('\n');
+
+	spin_unlock(&console_lock);
 	return 0;
 }
 
@@ -41,9 +60,8 @@ size_t printf(const char *str, ...)
 	};
 
 	static char buffer[buffer_size];
-	static uint32_t lock;
 
-	spin_lock(&lock);
+	spin_lock(&console_lock);
 
 	va_list args;
 	va_start(args, str);
@@ -53,7 +71,7 @@ size_t printf(const char *str, ...)
 	if (ret > buffer_size)
 		ret = buffer_size;
 	
-	puts_len(buffer, ret);
-	spin_unlock(&lock);
+	__puts_len(buffer, ret);
+	spin_unlock(&console_lock);
 	return ret;
 }
