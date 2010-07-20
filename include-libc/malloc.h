517fbccc47e29e20bdcc6fd2f99b0ce43041afef
Varun Sethi <Varun.Sethi@freescale.com>
2010-07-20 20:48:15 +0000

    Added realloc wrapper to include-libc/malloc.h.
    Earlier realloc could not be used as the wrapper function for mspace_realloc
    did not exist in this file.
    
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>

--- include-libc/malloc.h
+++ include-libc/malloc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Freescale Semiconductor, Inc.
+ * Copyright (C) 2009,2010 Freescale Semiconductor, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,6 +38,11 @@ static inline __attribute__((malloc)) void *malloc(size_t size)
 static inline __attribute__((malloc)) void *memalign(size_t align, size_t size)
 {
 	return mspace_memalign(libos_mspace, size, align);
+}
+
+static inline __attribute__((malloc)) void *realloc(void *ptr, size_t size)
+{
+	return mspace_realloc(libos_mspace, ptr, size);
 }
 
 static inline void free(void *ptr)
