94e57b70b59f517868a9f9e535140e1a995ab26a
Tudor Laurentiu <laurentiu.tudor@freescale.com>
2012-11-14 19:22:04 +0000

    Add support and simple API for hardware threads
    Add hardware thread startup code and a simple API
    for starting hardware threads and getting the ID of
    the current hardware thread.
    
    Signed-off-by: Laurentiu Tudor <Laurentiu.Tudor@freescale.com>

--- include/libos/mp.h
+++ include/libos/mp.h
@@ -29,6 +29,10 @@
 #ifndef LIBOS_MP_H
 #define LIBOS_MP_H
 
+#include <libos/core-regs.h>
+#include <libos/percpu.h>
+#include <libos/cpu_caps.h>
+
 static inline void mttmr(int reg, register_t val)
 {
 	asm volatile(".long 0x7c0003dc | "
@@ -46,5 +50,64 @@ static inline register_t mftmr(int reg)
 
 	return ret;
 }
+
+#if CONFIG_LIBOS_MAX_HW_THREADS > 1
+
+/** Get the id of the thread currently running on
+ *
+ * @return: unsigned holding the current thread id
+ */
+static inline unsigned get_hw_thread_id(void)
+{
+	if (!cpu_has_ftr(CPU_FTR_THREADS))
+		return 0;
+
+	return mfspr(SPR_TIR);
+}
+
+/** Start a the specified hardware thread on the core currently running on
+ *
+ * @param[in] tid id of the thread to start. Must not be the id of the current thread
+ * @param[in] msr initial value of MSR that the thread will start with.
+ * @param[in] cpu pointer to the cpu_t structure of the newly started thread.
+ * 		  Structure must be accessible (mapped in shared TLB1) in the
+ * 		  starting thread and the stack in cpu->kstack must be
+ * 		  initialized and pointing to stack[KSTACK_SIZE - FRAMELEN].
+ */
+static inline void start_hw_thread(unsigned int tid, uint32_t msr, cpu_t *cpu)
+{
+	void hw_thread_start(void);
+	extern cpu_t *hw_thread_percpus[MAX_CORES * CONFIG_LIBOS_MAX_HW_THREADS];
+
+	assert(tid != get_hw_thread_id());
+	assert(tid < cpu_caps.threads_per_core);
+
+	if (tid == 1) {
+		mttmr(TMR_IMSR1, msr);
+		mttmr(TMR_INIA1, (register_t)&hw_thread_start);
+		hw_thread_percpus[mfspr(SPR_PIR) + 1] = cpu;
+	} else if (tid == 0) {
+		mttmr(TMR_IMSR0, msr);
+		mttmr(TMR_INIA0, (register_t)&hw_thread_start);
+		hw_thread_percpus[mfspr(SPR_PIR) - 1] = cpu;
+	} else {
+		assert(0);
+	}
+	mtspr(SPR_TENS, 1 << tid);
+}
+
+#else
+
+static inline int get_hw_thread_id(void)
+{
+	return 0;
+}
+
+static inline void start_hw_thread(unsigned int tid, uint32_t msr, cpu_t *cpu)
+{
+	assert(0);
+}
+
+#endif
 
 #endif
