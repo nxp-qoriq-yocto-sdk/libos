nxp-qoriq-yocto-sdk/libos

commit 92d3ed867d3a3fcf2a0f02cd434af3ead0be2375

Replaced printfs with printlogs in libos
Libos code was cleaned up from printfs

Signed-off-by: Adrian Raileanu <adrian.raileanu@freescale.com>

lib/trap.c
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2007-2009 Freescale Semiconductor, Inc.
+ * Copyright (C) 2007-2011 Freescale Semiconductor, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -78,19 +78,20 @@ static const char *trapname(int vector)
 static void traceback(trapframe_t *regs)
 {
 	unsigned long *sp = ((unsigned long *)regs->gpregs[1]);
-	printf("sp %p %lx %lx %lx %lx\n", sp, sp[0], sp[1], sp[2], sp[3]);
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,
+	         "sp %p %lx %lx %lx %lx\n", sp, sp[0], sp[1], sp[2], sp[3]);
 	sp = ((unsigned long **)regs->gpregs[1])[0];
 
-	printf("Traceback: ");
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,"Traceback: ");
 
 	for (int i = 1; sp != NULL; i++, sp = (unsigned long *)sp[0]) {
 		if ((i % 7) == 0)
-			printf("\n");
+			printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,"\n");
 
-		printf("0x%08lx ", sp[1] - 4);
+		printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS, "0x%08lx ", sp[1] - 4);
 	}
 
-	printf("\n");
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,"\n");
 }
 
 void dump_regs(trapframe_t *regs)
@@ -104,20 +105,22 @@ void dump_regs(trapframe_t *regs)
 		saved = spin_lock_intsave(&dump_lock);
 	}
 
-	printf("%s\n", trapname(regs->exc));
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS, "%s\n", trapname(regs->exc));
 
-	printf("NIP 0x%08lx MSR 0x%08lx LR 0x%08lx ESR 0x%08lx EXC %d\n"
-	       "CTR 0x%08lx CR 0x%08x XER 0x%08x DEAR 0x%08lx PIR %lu\n"
-	       "Prev trap level %d\n",
-	       regs->srr0, regs->srr1, regs->lr, mfspr(SPR_ESR), regs->exc,
-	       regs->ctr, regs->cr, regs->xer, mfspr(SPR_DEAR), mfspr(SPR_PIR),
-	       regs->traplevel);
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,
+	         "NIP 0x%08lx MSR 0x%08lx LR 0x%08lx ESR 0x%08lx EXC %d\n"
+	         "CTR 0x%08lx CR 0x%08x XER 0x%08x DEAR 0x%08lx PIR %lu\n"
+	         "Prev trap level %d\n",
+	         regs->srr0, regs->srr1, regs->lr, mfspr(SPR_ESR), regs->exc,
+	         regs->ctr, regs->cr, regs->xer, mfspr(SPR_DEAR), mfspr(SPR_PIR),
+	         regs->traplevel);
 
 	for (int i = 0; i < 32; i++) {
-		printf("r%02d 0x%08lx  ", i, regs->gpregs[i]);
+		printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,
+		         "r%02d 0x%08lx  ", i, regs->gpregs[i]);
 
 		if ((i & 3) == 3)
-			printf("\n");
+			printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS, "\n");
 	}
 
 #ifdef HYPERVISOR
@@ -133,7 +136,8 @@ void dump_regs(trapframe_t *regs)
 
 void unknown_exception(trapframe_t *regs)
 {
-	printf("unknown exception: %s\n", trapname(regs->exc));
+	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,
+	         "unknown exception: %s\n", trapname(regs->exc));
 	dump_regs(regs); 
 	
 	stopsim();

example/src/init.c
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2009,2010 Freescale Semiconductor, Inc.
+ * Copyright (C) 2009-2011 Freescale Semiconductor, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -461,7 +461,8 @@ void init(unsigned long devtree_ptr)
 		if (!fdt_node_check_compatible(fdt, node, "ns16550"))
 			stdout = test_init_uart(node);
 		else {
-			printf("Unrecognized stdout compatible.\n");
+			printlog(LOGTYPE_DEV, LOGLEVEL_ERROR,
+			         "Unrecognized stdout compatible.\n");
 			stdout = NULL;
 		}
 
@@ -469,9 +470,11 @@ void init(unsigned long devtree_ptr)
 			console_init(stdout);
 		else
 			/* The message will at least go to the log buffer... */
-			printf("Failed to initialize stdout.\n");
+			printlog(LOGTYPE_MISC, LOGLEVEL_ERROR,
+			         "Failed to initialize stdout.\n");
 	} else {
-		printf("No stdout found.\n");
+		printlog(LOGTYPE_MISC, LOGLEVEL_ERROR,
+		         "No stdout found.\n");
 	}
 }
 

include/libos/bitops.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2010 Freescale Semiconductor, Inc.
+ * Copyright (C) 2007-2011 Freescale Semiconductor, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -118,7 +118,8 @@ static inline void spin_lock(uint32_t *ptr)
 
 	if (spin_lock_held(ptr)) {
 		set_crashing(1);
-		printf("Recursive spin_lock detected on %p\n", ptr);
+		printlog(LOGTYPE_MP, LOGLEVEL_ALWAYS,
+		         "Recursive spin_lock detected on %p\n", ptr);
 		set_crashing(0);
 		BUG();
 	}

lib/mpic.c
@@ -3,7 +3,7 @@
  */
 
 /*
- * Copyright (C) 2008-2010 Freescale Semiconductor, Inc.
+ * Copyright (C) 2008-2011 Freescale Semiconductor, Inc.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -266,8 +266,9 @@ void mpic_reset_core(void)
 	}
 
 	if (!mpic_coreint)
-		printf("mpic_reset_core(): too many interrupts, vector %x\n",
-		       vector);
+		printlog(LOGTYPE_IRQ, LOGLEVEL_ERROR,
+		         "mpic_reset_core(): too many interrupts, vector %x\n",
+		         vector);
 }
 
 static int error_int_p4080_rev1(void *arg)
@@ -275,10 +276,12 @@ static int error_int_p4080_rev1(void *arg)
 	interrupt_t *irq = arg;
 
 	set_crashing(1);
-	printf("Shared error interrupt received, EISR0: %#x\n",
-	       in32((uint32_t *)(CCSRBAR_VA + MPIC + MPIC_ERROR_INT_SUMMARY)));
-	printf("Future error interrupts will be disabled "
-	       "due to p4080 rev 1 limitations.\n");
+	printlog(LOGTYPE_IRQ, LOGLEVEL_ALWAYS,
+		"Shared error interrupt received, EISR0: %#x\n",
+	        in32((uint32_t *)(CCSRBAR_VA + MPIC + MPIC_ERROR_INT_SUMMARY)));
+	printlog(LOGTYPE_IRQ, LOGLEVEL_ALWAYS,
+	         "Future error interrupts will be disabled "
+	         "due to p4080 rev 1 limitations.\n");
 	set_crashing(0);
 
 	mpic_irq_mask(irq);

commit e830c9d750e62e580bafcd1bbc0973e17c157a5d

traps: Don't use printlog() for continuation lines
Otherwise you get CPU number annotations in the middle of the line,
which makes crash dumps harder to read.

Signed-off-by: Scott Wood <scottwood@freescale.com>
Fixes: 92d3ed867d3a3 ("Replaced printfs with printlogs in libos")

lib/trap.c
@@ -89,12 +89,12 @@ static void traceback(trapframe_t *regs)
 
 	for (int i = 1; sp != NULL; i++, sp = (unsigned long *)sp[0]) {
 		if ((i % 7) == 0)
-			printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,"\n");
+			printf("\n");
 
-		printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS, "0x%08lx ", sp[1] - 4);
+		printf("0x%08lx ", sp[1] - 4);
 	}
 
-	printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,"\n");
+	printf("\n");
 }
 
 void dump_regs(trapframe_t *regs)
@@ -119,11 +119,10 @@ void dump_regs(trapframe_t *regs)
 	         regs->traplevel);
 
 	for (int i = 0; i < 32; i++) {
-		printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS,
-		         "r%02d 0x%08lx  ", i, regs->gpregs[i]);
+		printf("r%02d 0x%08lx  ", i, regs->gpregs[i]);
 
 		if ((i & 3) == 3)
-			printlog(LOGTYPE_MISC, LOGLEVEL_ALWAYS, "\n");
+			printf("\n");
 	}
 
 #ifdef HYPERVISOR